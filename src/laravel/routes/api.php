20166381b1bd8bd79e39162ab9d33da7ddd163e5
Shiva Guru Balaji <s.shivaguru@fpmarkets.com.au>
2025-04-16 18:32:27 +0530

    feat: added sanctum for authentication

--- src/laravel/routes/api.php
+++ src/laravel/routes/api.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\AttendeeController;
 use App\Http\Controllers\BookingController;
 
-//Route::middleware('auth:sanctum')->group(function () {
-//    Route::apiResource('events', EventController::class);
-//    Route::apiResource('events.attendees', AttendeeController::class)->except(['store']);
-//    Route::post('events/{event}/book', [BookingController::class, 'store']);
-//});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('events', EventController::class);
+    Route::apiResource('events.attendees', AttendeeController::class)->except(['store']);
+    Route::post('events/{event}/book', [BookingController::class, 'store']);
+});
 
 Route::post('/events/{event}/attendees', [AttendeeController::class, 'store']);
 
